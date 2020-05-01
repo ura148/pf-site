8dfce2c7064877b6bd48638ece57cd5db9e09e51
ura148 <yuki.ura10@gmail.com>
2020-05-01 15:21:36 +0900

    portfolio area のcontent のレイアウトを変更

---
+++ js/style.js
@@ -0,0 +1,39 @@
+
+
+$(window).on('load resize',function(){
+  let pfList = $("#pf-list"),
+      pfContent = $(".pf-content"),
+      pfContent2 = $(".pf-content2"),
+      pfListWidth = pfList.width(),
+      pfListHeight = pfListWidth * 1.68 ,
+      pfContentHeight = pfContent.height(),
+      pfContent2Height = pfContent2.height();
+      
+  // pf area のサイズ指定
+  $(".pf-list").css("height",pfListHeight);
+  pfContent.css("padding-top", (pfListHeight - pfContentHeight) / 2),
+  pfContent.css("padding-bottom", (pfListHeight - pfContentHeight) / 2),
+  pfContent2.css("padding-top", (pfListHeight - pfContent2Height) / 2),
+  pfContent2.css("padding-bottom", (pfListHeight - pfContent2Height) / 2);        
+});
+
+$(function(){
+  $(window).scroll(function(){
+    let scrollTop = $(window).scrollTop(),
+        windowHeight = $(window).height(),
+        scrollBottom = scrollTop + windowHeight;
+
+    $(".fade-animation").each(function(){
+      let itemTop = $(this).offset().top,
+          itemHeight =$(this).height(),
+          fadeStart = itemTop + (itemHeight / 2);
+
+          console.log(itemTop),
+          console.log(fadeStart);
+
+          if (scrollTop > fadeStart - windowHeight){
+            $(this).addClass('fade-in');
+          }
+    });
+  });
+});
