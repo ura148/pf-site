c81bf7cafd8b12407e30749576cbdf9c13214b21
ura148 <yuki.ura10@gmail.com>
2020-05-01 17:16:29 +0900

    blog area のレイアウト調整

--- js/style.js
+++ js/style.js
@@ -6,15 +6,19 @@ $(window).on('load resize',function(){
       pfContent2 = $(".pf-content2"),
       pfListWidth = pfList.width(),
       pfListHeight = pfListWidth * 1.68 ,
+      pfListheight2 = pfListWidth / 1.618  ,
       pfContentHeight = pfContent.height(),
       pfContent2Height = pfContent2.height();
       
   // pf area のサイズ指定
-  $(".pf-list").css("height",pfListHeight);
+  $(".pf-list").css("height",pfListHeight),
   pfContent.css("padding-top", (pfListHeight - pfContentHeight) / 2),
   pfContent.css("padding-bottom", (pfListHeight - pfContentHeight) / 2),
   pfContent2.css("padding-top", (pfListHeight - pfContent2Height) / 2),
-  pfContent2.css("padding-bottom", (pfListHeight - pfContent2Height) / 2);        
+  pfContent2.css("padding-bottom", (pfListHeight - pfContent2Height) / 2);
+  $(".content3").css("height",pfListheight2);
+          
+          
 });
 
 $(function(){
@@ -27,9 +31,6 @@ $(function(){
       let itemTop = $(this).offset().top,
           itemHeight =$(this).height(),
           fadeStart = itemTop + (itemHeight / 2);
-
-          console.log(itemTop),
-          console.log(fadeStart);
 
           if (scrollTop > fadeStart - windowHeight){
             $(this).addClass('fade-in');
