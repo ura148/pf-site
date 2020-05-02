dd328d261f15b1b17409881547dcd3f8be03a447
ura148 <yuki.ura10@gmail.com>
2020-05-02 15:42:49 +0900

    sp 版　portfolio のレイアウト及びul要素の高さを指定

--- js/style.js
+++ js/style.js
@@ -1,23 +1,36 @@
 
 
 $(window).on('load resize',function(){
-  let pfList = $("#pf-list"),
+  let deviceW = 768,
+      windowW = $(window).width(),
+      pfItem = $(".pf-item"),
       pfContent = $(".pf-content"),
       pfContent2 = $(".pf-content2"),
-      pfListWidth = pfList.width(),
-      pfListHeight = pfListWidth * 1.68 ,
-      pfListheight2 = pfListWidth / 1.618  ,
+      pfItemWidth = pfItem.width(),
+      pfItemHeight = pfItemWidth * 1.68 ,
+      pfItemheight2 = pfItemWidth / 1.618  ,
       pfContentHeight = pfContent.height(),
       pfContent2Height = pfContent2.height();
 
-  // pf area のサイズ指定
-  $(".pf-list").css("height",pfListHeight),
-  pfContent.css("padding-top", (pfListHeight - pfContentHeight) / 2),
-  pfContent.css("padding-bottom", (pfListHeight - pfContentHeight) / 2),
-  pfContent2.css("padding-top", (pfListHeight - pfContent2Height) / 2),
-  pfContent2.css("padding-bottom", (pfListHeight - pfContent2Height) / 2);
-  $(".content3").css("height",pfListheight2);
+      // pf area のサイズ指定
+      $(".pf-item").css("height",pfItemHeight),
+      pfContent.css("padding-top", (pfItemHeight - pfContentHeight) / 2),
+      pfContent.css("padding-bottom", (pfItemHeight - pfContentHeight) / 2),
+      pfContent2.css("padding-top", (pfItemHeight - pfContent2Height) / 2),
+      pfContent2.css("padding-bottom", (pfItemHeight - pfContent2Height) / 2);
+      $(".content3").css("height",pfItemheight2);
 
+      if(windowW <= deviceW){
+        let portfolioLi = $(".portfolio-list"),
+            portfolioItemF = $("#portfolioItemF"),
+            portfolioitemE = $("#portfolioItemE"),
+            portfolioItemFTop = $("#portfolioItemF").offset().top,
+            portfolioItemETop = $("#portfolioItemE").offset().top,
+            portfolioItemEHeight = $("#portfolioItemE").height(),
+            portfolioItemEBottom = portfolioItemETop + portfolioItemEHeight;
+
+        portfolioLi.css("height",(portfolioItemEBottom - portfolioItemFTop));
+      }
 
 });
 
